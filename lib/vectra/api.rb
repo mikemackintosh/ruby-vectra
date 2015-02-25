23cd922d06312c4d7b6e3b040fd1d07baafb4d5e
Mike Mackintosh <m@zyp.io>
2015-02-25 09:46:21 -0500

    get all hosts

--- lib/vectra/api.rb
+++ lib/vectra/api.rb
@@ -1,18 +1,43 @@
 module Vectra
   class Api
+    class InvalidResponse < RuntimeError ; end
 
     include HTTParty
 
-    def self.request
+    def self.send(url, args="")
+      results = []
+
+      # Send the request
       response = HTTParty.get( 
-        "#{Vectra::Config.endpoint}#{self.target}",
-        :verify => false, 
+        "#{url}#{args}",
+        :verify => false, # Vectra doesnt accept real certs yet
         :basic_auth => { 
           :username => Vectra::Config.username, 
           :password => Vectra::Config.password
       })
 
-      puts response.inspect
+      if !response.code.eql?(200)
+        raise Vectra::Api::InvalidResponse, "Invalid Response Received"
+      end
+
+      # Decode the response
+      response = JSON.parse response.body
+
+      # Map responses
+      response['results'].map{|r| results.push(r)}
+
+      # If there is a next, respect it
+      #unless response['next'].nil?
+      #  puts self.send(response['next']).inspect
+      #end
+
+      results
+
+    end
+
+    def self.request
+      r = self.send("#{Vectra::Config.endpoint}#{self.target}")
+      puts r.inspect
     end
 
   end
